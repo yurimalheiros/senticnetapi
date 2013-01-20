9d8b55c1d7acb11d11ecf1c8e5861ed6b2fd6007
yurimalheiros <yurimalheiros@gmail.com>
2013-01-20 14:44:25 -0300

    using the new sentic api

--- senticnet/senticnet.py
+++ senticnet/senticnet.py
@@ -1,22 +1,79 @@
 import rdflib
+from rdflib.term import URIRef
 
 class Senticnet(object):
     """
     Ultra simple API to use Senticnet without be bothered by RDF stuff.
     """
     def __init__(self):    
+        self.base_url = "http://sentic.net/api/en/concept/"
+
+    def concept(self, concept):
         graph = rdflib.Graph()
-        self.senticnet = graph.parse("senticnet.rdf.xml")
+        parsed_graph = graph.parse(self.base_url+concept, format="xml")
+        result = {}
 
-    def get_polarity(self, text):
-        """
-        Get the polarity of a text using Senticnet. If the word is not defined
-        in the Senticnet the function returns zero.
-        """
-        subject_uri = "http://openmind.media.mit.edu/api/en/concept/%s" % text.replace(" ", "%20")
-        polarity_uri = "http://www.semedia.dibet.univpm.it/heo/heo#polarity"
+        result["polarity"] =  self._get_polarity(concept, parsed_graph)
+        result["sentics"] = self._get_sentics(concept, parsed_graph)
+        result["semantics"] = self._get_semantics(concept, parsed_graph)
 
-        obj = self.senticnet.value(subject=rdflib.term.URIRef(subject_uri),
-                                   predicate=rdflib.term.URIRef(polarity_uri))
+        return result
 
-        return float(obj.title()) if obj is not None else 0
+    def polarity(self, concept):
+        return self._get_polarity(concept)
+
+    def sentics(self, concept):
+        return self._get_sentics(concept)
+
+    def semantics(self, concept):
+        return self._get_semantics(concept)
+
+    def _get_semantics(self, concept, parsed_graph=None):
+        """Return the semantics associated with the concept"""
+        concept_semantics_uri = self.base_url+concept+"/semantics"
+        semantics_predicate_uri = "http://sentic.net/api/semantics"
+
+        if parsed_graph is None:
+            g = rdflib.Graph()
+            parsed_graph = g.parse(concept_semantics_uri, format="xml")
+
+        objects = parsed_graph.objects(predicate=URIRef(semantics_predicate_uri))
+        return [self._last_uri_element(o.toPython()) for o in objects]
+
+    def _get_sentics(self, concept, parsed_graph=None):
+        """Return sentics of a concept"""
+        concept_sentics_uri = self.base_url+concept+"/sentics"
+        senticnet_api_url = "http://sentic.net/api/"
+        sentics = {"pleasantness" : 0, "attention" : 0, "sensitivity" : 0, "aptitude" : 0}
+
+        if parsed_graph is None:
+            graph = rdflib.Graph()
+            parsed_graph = graph.parse(concept_sentics_uri, format="xml")
+
+        for sentic in sentics.keys():
+            sentics[sentic] = parsed_graph.objects(predicate=URIRef(senticnet_api_url+sentic)).next().toPython()
+
+        return sentics
+
+    def _get_polarity(self, concept, parsed_graph=None):
+        """Return the polarity of a concept"""
+        concept_polarity_uri = self.base_url+concept+"/polarity"
+        senticnet_api_url = "http://sentic.net/api/"
+        predicate_uri = senticnet_api_url+"polarity"
+
+        if parsed_graph is None:
+            graph = rdflib.Graph()
+            parsed_graph = graph.parse(concept_polarity_uri, format="xml")
+
+        return parsed_graph.objects(predicate=URIRef(predicate_uri)).next().toPython()
+
+    def _last_uri_element(self, uri):
+        return uri.split("/")[-1]
+
+
+if __name__ == '__main__':
+    sn = Senticnet()
+    print sn.concept('love')
+    print sn.polarity('love')
+    print sn.sentics('love')
+    print sn.semantics('love')
